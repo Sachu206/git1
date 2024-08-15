570de05a7982d24f3c044c01014bdfc112e73dd8
Praveen <Praveen@LAPTOP-FJ727NGV.mshome.net>
2024-08-15 14:41:19 +0530

    push and commit

---
+++ src/git1/demo1.java
@@ -0,0 +1,20 @@
+package git1;
+
+
+public class demo1
+{
+	public static void main(String[]args)
+	{
+		int fib1=1;
+		int fib2=1;
+		int fib3;
+		for (int i=0;i<=10;i++)
+		{
+			fib3=fib1*fib2;
+			
+			System.out.println(fib3+" ");
+		}
+		
+		
+	}
+}
